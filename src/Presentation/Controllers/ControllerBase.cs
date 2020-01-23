7ec77698a6bb13d996329c892f25538f5e97a333
r-stewart <r.sttewart@corgihomeplan.co.uk>
2020-01-23 18:56:29 +0000

    added base controller

---
+++ src/Presentation/Controllers/ControllerBase.cs
@@ -0,0 +1,14 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public abstract class ControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
